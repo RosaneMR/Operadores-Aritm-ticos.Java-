4222b527c68b2009f2375668f1728843a08ea7af
Rosane Maciel <rosanemarciel@gmail.com>
2022-06-12 18:17:10 -0300

    first commit

---
+++ src/OperadoresAritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,11 @@
+package OperadoresAritimeticos ;
+
+public class OperadoresAritimeticos {
+    public static void main(String[]args){
+
+        int n1= 3;
+        int n2= 5;
+        float m=(n1+n2)/2;
+        System.out.println("Á média é igual a "+ m);
+    }
+}
